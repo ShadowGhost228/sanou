e3b0716997ebdba10bec923549bb092562744b10
Richy <kuehomsi@et.esiea.fr>
2018-09-08 21:53:54 +0200

    update node js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const list = [{
+    name: 'elem1'
+}, {
+    name: 'elem2'
+}]
+
+router.get('/list', (req, res) => {
+    res.json(list)
+})
+
+router.post('/list', (req, res) => {
+    list.push({
+        name: req.body.name
+    })
+    res.send('OK')
+})
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
 
 module.exports = router;
